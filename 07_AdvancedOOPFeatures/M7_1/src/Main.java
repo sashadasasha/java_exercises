4152e34ee955b2affc562a96de7a205a49ce8ec5
Sasha Korn <you@example.com>
2020-03-31 17:25:00 +0300

    Сортировка по зарплате и именам

---
+++ 07_AdvancedOOPFeatures/M7_1/src/Main.java
@@ -0,0 +1,41 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+    private static String staffFile = "java_basics/07_AdvancedOOPFeatures/M7_1/data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args) {
+        ArrayList<Employee> staff = loadStaffFromFile();
+        Collections.sort(staff, Comparator.comparing(Employee::getName));
+        Collections.sort(staff, Comparator.comparing(Employee::getSalary));
+
+        for (Employee empl: staff) {
+            System.out.println(empl);
+        }
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile() {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for (String line : lines) {
+                String[] fragments = line.split("\t");
+                if (fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
