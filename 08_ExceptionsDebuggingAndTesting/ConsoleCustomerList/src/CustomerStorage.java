39c6e1babc2a323a144af620b59c6bf63c84e357
Sasha Korn <you@example.com>
2020-04-03 13:11:45 +0300

    Проверка email и phone

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage
 {
@@ -12,7 +14,24 @@ public class CustomerStorage
     public void addCustomer(String data)
     {
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong format! Correct format: \n" +
+                    "add Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
         String name = components[0] + " " + components[1];
+        Pattern patternEmail = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Pattern patternPhone = Pattern.compile("^\\+7[0-9]{10}$");
+        Matcher matcher;
+        matcher = patternEmail.matcher(components[2]);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Wrong format of email! Correct format: \n" +
+                    "vasily.petrov@gmail.com");
+        }
+        matcher = patternPhone.matcher(components[3]);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Wrong format of phone number! Correct format: \n" +
+                    "+79215637722");
+        }
         storage.put(name, new Customer(name, components[3], components[2]));
     }
 
