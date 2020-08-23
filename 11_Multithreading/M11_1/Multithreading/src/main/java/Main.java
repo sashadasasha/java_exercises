64de4cf35e82e06c38c6f5c24037d7ba667f5f0a
Aleksandra Kornyukhina <sashadasasha@gmail.com>
2020-08-23 22:45:15 +0300

    module11_1

--- 11_Multithreading/M11_1/Multithreading/src/main/java/Main.java
+++ 11_Multithreading/M11_1/Multithreading/src/main/java/Main.java
@@ -1,5 +1,38 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Runtime.getRuntime().availableProcessors());
+        String srcFolder = "C:\\Users\\Sasha\\Desktop\\src";
+        String dstFolder = "C:\\Users\\Sasha\\Desktop\\dst";
+
+        File srcDir = new File(srcFolder);
+
+        int processes = Runtime.getRuntime().availableProcessors();
+        List<File> files = Arrays.asList(srcDir.listFiles());
+        List<List<File>> listOfLists = new ArrayList<>();
+
+        for(int i = 0; i < processes; i ++) {
+            List<File> list = new ArrayList<>();
+            listOfLists.add(list);
+        }
+
+        for (int i = 0; i < files.size(); i ++) {
+            if (i < processes) {
+                listOfLists.get(i).add(files.get(i));
+                continue;
+            } else {
+                listOfLists.get(i % processes).add(files.get(i));
+                continue;
+            }
+        }
+
+        long start = System.currentTimeMillis();
+        for (List<File> f : listOfLists) {
+            ImageResizer imageResizer = new ImageResizer(dstFolder, 300, f, start);
+            new Thread(imageResizer).start();
+        }
     }
 }
